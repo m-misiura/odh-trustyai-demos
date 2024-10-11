1fac476fdb58489d566d4d27c9bc38681b8e935c
m-misiura <mmisiura@redhat.com>
2024-10-11 17:47:58 +0100

    :memo: updating tokens and namespaces for consistency with the previous demo

--- 2-BiasMonitoring/kserve-demo/scripts/apply_name_mapping.sh
+++ 2-BiasMonitoring/kserve-demo/scripts/apply_name_mapping.sh
@@ -1,6 +1,7 @@
 MODEL_ALPHA=demo-loan-nn-onnx-alpha
 MODEL_BETA=demo-loan-nn-onnx-beta
 TRUSTY_ROUTE=https://$(oc get route/trustyai-service --template={{.spec.host}})
+TOKEN=$(oc create token user-one)
 
 for model in $MODEL_ALPHA $MODEL_BETA; do
   curl -sk  -H "Authorization: Bearer ${TOKEN}" -X POST --location $TRUSTY_ROUTE/info/names \
